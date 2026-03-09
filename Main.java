e1d7f58a5ce50e0956081d76731fa30d5802f250
Feriel-626 <ferielboubeggar@gmail.com>
2026-03-09 18:26:06 +0100

    hello world

--- Main.java
+++ Main.java
@@ -7,4 +7,6 @@ public class Main {
     System.out.println("age 20 ans !");
     System.out.println("specialite isil");
     System.out.println("groupe 3");
+    System.out.println("hello world");
+    
     }}
